66d483ab311ec794dbf7b62308b3222504c60ca2
JulianR <julianriom2013@gmail.com>
2018-11-13 16:24:26 -0300

    Nuevos cambios agregados

--- src/App.js
+++ src/App.js
@@ -3,9 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 
 import Navigation from './components/navigation';
+import {tareas} from './tareas.json';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.tasks = {
+      tareas
+    }
+  }
+
   render() {
+    console.log(this.tasks.tareas);
     return (
       <div className="App">
           <Navigation />
